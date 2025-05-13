a9baf25107ff86eec62fd4a32033cffac490d7fc
Luis Medina <luis.m.medina@oracle.com>
2025-05-13 00:38:42 -0600

    ZSH

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -24,3 +24,23 @@ alias gsc='git branch --show-current | pbcopy'
 # Directory stack shortcuts
 alias d='dirs -v'
 for index ({1..9}); do alias "$index"="cd +$index"; done
+
+# Function to reload SSH keys with Yubikey
+reload-ssh() {
+  if command -v ssh-add >/dev/null; then
+    ssh-add -e /usr/local/lib/opensc-pkcs11.so > /dev/null
+    if [[ $? -gt 0 ]]; then echo "Failed to remove previous card"; fi
+    ssh-add -s /usr/local/lib/opensc-pkcs11.so
+  fi
+}
+
+# Activate Python virtual environments
+# https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/
+function activate-venv() {
+  local selected_env
+  selected_env=$(ls ~/.venv/ | fzf)
+
+  if [ -n "$selected_env" ]; then
+    source "$HOME/.venv/$selected_env/bin/activate"
+  fi
+}
--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -15,6 +15,8 @@ setopt sharehistory       # Share command history across sessions
 setopt histignoredups     # Ignore duplicate commands in history
 setopt incappendhistory   # Write to history immediately
 setopt hist_ignore_all_dups hist_save_no_dups hist_find_no_dups histignorespace histverify extendedhistory
+setopt hist_verify        # Do not execute immediately upon history expansion.
+setopt hist_ignore_space  # Do not record an event starting with a space.
 
 # Shell behavior
 setopt correct            # Auto-correct minor command misspellings
@@ -43,3 +45,14 @@ backward_delete_word() {
 }
 zle -N backward_delete_word
 bindkey '^W' backward_delete_word
+
+# History options
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+
+# Add files and directories color when running ls
+# There's a generator here: http://geoff.greer.fm/lscolors/
+export CLICOLOR=1
+export LS_COLORS='di=36:ln=1;35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+export GREP_COLOR='1;35;40'
