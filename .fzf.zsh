5be4da2d696afec4d4bae92a982afc64645f1cde
Luis Medina <luis.m.medina@oracle.com>
2025-05-14 19:35:44 -0600

    Update bootstrap

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,40 @@
 # Setup fzf
 # ---------
+# Rebind ALT-c to CTRL-e
+# bindkey -rM emacs '\ec'
+# bindkey -rM vicmd '\ec'
+# bindkey -rM viins '\ec'
+
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 source <(fzf --zsh)
+
+_fzf_comprun() {
+    local command=$1
+    shift
+
+    case "$command" in
+        cd)           find . -type d | fzf --preview 'tree -C {}' "$@";;
+        *)            fzf "$@" ;;
+    esac
+}
+
+_fzf_compgen_path() {
+    rg --files --glob "!.git" "$1"
+}
+
+_fzf_compgen_dir() {
+   fd --type d --hidden --follow --exclude ".git" "$1"
+}
+
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    tree)         find . -type d | fzf --preview 'tree -C {}' "$@";;
+    *)            fzf "$@" ;;
+  esac
+}
