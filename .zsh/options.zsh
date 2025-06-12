c3865a8490441600b19cde6473d82b165336531a
Luis Medina <luis.m.medina@oracle.com>
2025-06-12 17:32:05 -0600

    Edit custom word functions

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -23,33 +23,55 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
 # Custom word functions
-backward_move_word() {
-  local WORDCHARS='_'
+remove-delimiters() {
+  # Copy the global WORDCHARS variable to a local variable. 
+  local WORDCHARS=$WORDCHARS
+  WORDCHARS="${WORDCHARS//:}"
+  WORDCHARS="${WORDCHARS//\/}"
+  WORDCHARS="${WORDCHARS//.}"
+  echo "$WORDCHARS"
+}
+
+my-backward-move-word() {
+  local WORDCHARS=$(remove-delimiters)
   zle backward-word
 }
 
-forward_move_word() {
-  local WORDCHARS='_'
+my-forward-move-word() {
+  local WORDCHARS=$(remove-delimiters)
   zle forward-word
 }
 
-backward_delete_word() {
-  local WORDCHARS='_'
+# New default `ctrl+w` command
+my-backward-delete-word() {
+  
+  local WORDCHARS=$(remove-delimiters)
+  # zle <widget-name> will run an existing widget.
   zle backward-kill-word
 }
 
-# Register functions with ZLE 
-zle -N backward_delete_word
-zle -N backward_move_word
-zle -N forward_move_word
+# This will be our `ctrl+alt+w` command
+my-backward-delete-whole-word() {
+  local WORDCHARS=$WORDCHARS
+  # Add `:` to WORDCHARS if it's not present
+  [[ ! $WORDCHARS == *":"* ]] && WORDCHARS="$WORDCHARS"":"
+  zle backward-kill-word
+}
+
+# Register functions with ZLE
+zle -N my-backward-move-word
+zle -N my-forward-move-word
+zle -N my-backward-delete-word
+zle -N my-backward-delete-whole-word
 
 # Key bindings
-bindkey "^[b" backward_move_word     # VSCode Option+Left
-bindkey "^[f" forward_move_word      # VSCode Option+Right
-bindkey "^[[1;3D" backward_move_word # iTerm2 Option+Left
-bindkey "^[[1;3C" forward_move_word  # iTerm2 Option+Right
+bindkey "^[b" my-backward-move-word     # VSCode Option+Left
+bindkey "^[f" my-forward-move-word      # VSCode Option+Right
+bindkey "^[[1;3D" my-backward-move-word # iTerm2 Option+Left
+bindkey "^[[1;3C" my-forward-move-word  # iTerm2 Option+Right
 
-bindkey '^W' backward_delete_word
+bindkey '^W' my-backward-delete-word
+# bindkey '^[^W' my-backward-delete-whole-word
 
 bindkey '^E' end-of-line
 bindkey '^A' beginning-of-line
